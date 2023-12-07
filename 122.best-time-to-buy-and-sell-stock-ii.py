7ecce1d8dc112bf9edccff663d19df998fc3244e
Dae-Ki Kang <dkkang@gmail.com>
2023-12-07 16:21:03 +0900

    Create 122.best-time-to-buy-and-sell-stock-ii.py

---
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        r=0
+        for i in range(len(prices)-1):
+            if prices[i+1]>prices[i]:
+                r+=(prices[i+1]-prices[i])
+        return r
+        
